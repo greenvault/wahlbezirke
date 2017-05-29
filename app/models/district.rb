05484eb2c18367008418ffeea6267696f8d0c4d3
Jonas Knipper <jonas@robocomobo.com>
2017-05-29 09:15:11 +0200

    Switch from district table to cells

--- app/models/district.rb
+++ app/models/district.rb
@@ -1,4 +1,8 @@
 class District < ApplicationRecord
   belongs_to :state
   default_scope { order(district_identifier: :asc) }
+
+  def municipalities
+    Municipality.where(district: self)
+  end
 end

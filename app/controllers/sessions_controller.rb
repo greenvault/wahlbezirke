126d626711cb4843849e377ecf6a2d5dbc07a4cc
Jonas Knipper <jonas@robocomobo.com>
2017-06-22 15:11:21 +0200

    Change flash message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < Devise::SessionsController
   def create
     self.resource = warden.authenticate!(auth_options)
-    set_flash_message!(:success, :signed_in)
+    flash[:success] = 'Anmeldung erfolgreich.'
     sign_in(resource_name, resource)
     yield resource if block_given?
     respond_with resource, location: after_sign_in_path_for(resource)
